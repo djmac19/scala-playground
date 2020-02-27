7ad8d6fea37d332bb004e3b7095aa46c887621e3
Daniel.james.Mcentee <daniel.james.mcentee@wejoM-2271.local>
2020-02-27 14:35:39 +0000

    adding exercise on options and algebraic data types

--- src/main/scala/com/github/djmac19/Main.scala
+++ src/main/scala/com/github/djmac19/Main.scala
@@ -1,7 +1,38 @@
 package com.github.djmac19
+import com.github.djmac19.Main.Monday
+import com.github.djmac19.Main.Tuesday
+import com.github.djmac19.Main.Wednesday
+import com.github.djmac19.Main.Thursday
+import com.github.djmac19.Main.Friday
 
 object Main extends App {
     def genList(i: Int): List[Int] = 
         (0 to i).toList
+
+
+    def isEven(x: Int): Option[String] = 
+        if (x % 2 == 0) {
+            Some("this is even")
+        } else {
+            None
+        }
+        
+    sealed trait Weekdays
+    case object Monday extends Weekdays
+    case object Tuesday extends Weekdays
+    case object Wednesday extends Weekdays
+    case object Thursday extends Weekdays
+    case object Friday extends Weekdays
+
+    def outputWeekdayString(day: Weekdays): String =
+        day match {
+            case Monday => "Monday"
+            case Tuesday => "Tuesday"
+            case Wednesday => "Wednesday"
+            case Thursday => "Thursday"
+            case Friday => "Friday"
+        }
+
+
     
 }
--- src/test/scala/com/github/djmac19/MainSpec.scala
+++ src/test/scala/com/github/djmac19/MainSpec.scala
@@ -6,15 +6,38 @@ import com.github.djmac19.Main._
 
 class MainSpec extends AnyFunSpec {
     describe("genList") {
-        it("should generate a list if whatever...") {
+        it("should generate a list of all integers between 1 and i (inclusive)") {
             val expectedResult: List[Int] = 
                 List(1, 2, 3, 4, 5)
 
             val actualResult: List[Int] = 
                 genList(5)
-            
+
             expectedResult === actualResult
         }
 
+    }
+    describe("isEven") {
+        it("should return 'this is even' when passed an even number") {
+            
+            val expectedResult = "this is even"
+
+            val optActualResult: Option[String] = isEven(10)
+
+            val actualResult =  optActualResult match {
+                case Some(value) => value
+                case None =>    fail
+            }
+
+            actualResult === expectedResult
+
+        }
+    }
+    describe("outputWeekdayString") {
+        it("should return 'Monday' when passed the weekday Monday") {
+            val expectedResult = "Monday"
+            val actualResult = outputWeekdayString(Monday)
+            actualResult === expectedResult
+        }
     }
 }
